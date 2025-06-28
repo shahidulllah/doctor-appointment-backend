7500810b4082b691e011e7634af35c8f3280690f
shahidulllah <mdshahidsumon177@gmail.com>
2025-06-28 21:05:03 +0600

    main route added

--- src/app.ts
+++ src/app.ts
@@ -2,15 +2,19 @@ import express from "express";
 import cors from "cors";
 import authRoutes from "./routes/auth.routes";
 import serviceRoutes from "./routes/service.route";
+import availabilityRoutes from "./routes/availability.routes";
 
 export const app = express();
 
 app.use(cors());
 app.use(express.json());
 
+//Aplication routes
 app.use("/auth", authRoutes);
 app.use("/doctor/services", serviceRoutes);
+app.use("/doctor/availability", availabilityRoutes);
 
+//Checking
 app.get("/", (req, res) => {
   res.send("Doctor–Patient Appointment API is running");
 });
