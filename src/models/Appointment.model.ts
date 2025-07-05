d915bf5cca96d1c823fd5b5f899ad36ef0f9429c
shahidulllah <mdshahidsumon177@gmail.com>
2025-07-05 21:53:26 +0600

    working on reveiw implementing

--- src/models/Appointment.model.ts
+++ src/models/Appointment.model.ts
@@ -1,4 +1,4 @@
-import mongoose, { Schema} from "mongoose";
+import mongoose, { Schema } from "mongoose";
 import { IAppointment } from "../types/appointment.type";
 
 const AppointmentSchema = new Schema<IAppointment>(
@@ -6,13 +6,18 @@ const AppointmentSchema = new Schema<IAppointment>(
     doctorId: { type: Schema.Types.ObjectId, ref: "User", required: true },
     patientId: { type: Schema.Types.ObjectId, ref: "User", required: true },
     serviceId: { type: Schema.Types.ObjectId, ref: "Service", required: true },
-    selectedDate: { type: String, required: true }, // e.g., "2025-07-01"
+    selectedDate: { type: String, required: true },
     timeSlot: { type: String, required: true },
     status: {
       type: String,
       enum: ["pending", "accepted", "cancelled", "completed"],
       default: "pending",
     },
+    review: {
+      rating: { type: Number, min: 1, max: 5 },
+      comment: { type: String },
+    },
+    doctorNote: { type: String },
   },
   { timestamps: true }
 );
--- src/types/appointment.type.ts
+++ src/types/appointment.type.ts
@@ -13,4 +13,11 @@ export interface IAppointment extends Document {
   selectedDate: string;
   timeSlot: string;
   status: AppointmentStatus;
+  review: {
+    rating: number;
+    comment: string;
+  };
+  doctorNote: string;
+  createdAt: Date;
+  updatedAt: Date;
 }
