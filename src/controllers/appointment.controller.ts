7981386114aaee0f9ea4084967c9107d9d8c4c85
shahidulllah <mdshahidsumon177@gmail.com>
2025-07-05 22:04:35 +0600

    review adding

--- src/controllers/appointment.controller.ts
+++ src/controllers/appointment.controller.ts
@@ -71,3 +71,35 @@ export const getPatientAppointments = async (req: Request, res: Response) => {
     res.status(500).json({ error: "Failed to get appointments" });
   }
 };
+
+//Review adding
+export const addReview = async (req: Request, res: Response) => {
+  try {
+    const patientId = (req as any).user.userId;
+    const { id } = req.params; // appointment ID
+    const { rating, comment } = req.body;
+
+    const appointment = await AppointmentModel.findOne({
+      _id: id,
+      patientId,
+      status: "completed",
+    });
+
+    if (!appointment) {
+      return res
+        .status(404)
+        .json({ error: "Completed appointment not found or not yours" });
+    }
+
+    if (appointment.review?.rating) {
+      return res.status(400).json({ error: "Review already submitted" });
+    }
+
+    appointment.review = { rating, comment };
+    await appointment.save();
+
+    res.json({ message: "Review submitted", appointment });
+  } catch (error) {
+    res.status(500).json({ error: "Failed to submit review" });
+  }
+};
